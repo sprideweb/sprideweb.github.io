1354f253808df96459b7f37dbf44d203284faa93
sprideweb <22722033@bjtu.edu.cn>
2024-12-25 13:24:21 +0800

    Update data.js

--- e/data.js
+++ e/data.js
@@ -10,20 +10,21 @@ newsListList=`
 	    <div class="news">
 	    <div class="news-top-s">新闻列表</div>
 	        <nodata>没有内容</nodata>
-	        <!--<a onclick="goN(420)">学习宝启用新域名<t>2024/12/19</t></a>-->
+	        <!--<a onclick="goN(number)">Title<t>YYYY/MM/DD</t></a>-->
 	    </div>
 	    <div class="news">
 	    <div class="news-top-s">公告列表</div>
-	        <nodata>没有内容</nodata>
+	        <!--<nodata>没有内容</nodata>-->
+	        <a onclick="goN(1097)">春潮网暂停访问<t>2024/12/25</t></a>
 	    </div>
 	    <br/><br/><br/><br/>
     </div>
 `;
 function newsInfoInfo() {
 	switch(nid) {
-	case '0':
-		newsCC=``;
-		basI('网站创建成功',100,100,100);
+	case '1097':
+		newsCC=`由于新版网站出现错误，春潮网spride.asia主页正在降级至旧版网站。恢复时间待定。`;
+		basI('春潮网暂停访问',2024,12,25);
 		break;
 	default:
 		window.location.href="#/index/newsList";
